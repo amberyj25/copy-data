888e7263bb6c1294d8f5ff6a6dba23615a29c4af
amberyj25 <amber20240@gmail.com>
2024-01-27 16:54:01 +0800

    create copyData project

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+        string fileAPath = Path.Combine(desktopPath, "data.txt");
+        string fileBPath = Path.Combine(desktopPath, "dataCopy.txt");
+
+        if (File.Exists(fileAPath))
+        {
+            try
+            {
+                File.Copy(fileAPath, fileBPath, true);
+                Console.WriteLine($"已將 {fileAPath} 的內容複製到 {fileBPath}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"沒有複製");
+                throw;
+            }
+        }
+        else
+        {
+            Console.WriteLine($"找不到檔案 {fileAPath}。");
+        }
+    }
+}
